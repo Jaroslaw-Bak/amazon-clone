3c5201f7126812dfd126b3a92ada5c317088b29a
Jarosław Bąk <jarodelphi@gmail.com>
2022-05-11 17:52:57 +0200

    firebase init, deploy

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,12 +6,13 @@ import Subtotal from'./Subtotal'
 
 
 function Checkout() {
-    const [{basket}, dispatch] = useStateValue()
+    const [{basket, user}, dispatch] = useStateValue()
   return (
     <div className='checkout'>
         <div className='checkout__left'>
             { /* <img className='checkout__ad' src="#" alt="add" /> */ }
             <div>
+                <h3>Hello, {user?._delegate.email}</h3>
                 <h2 className='checkout__title'>
                     Your shopping Basket
                 </h2>
