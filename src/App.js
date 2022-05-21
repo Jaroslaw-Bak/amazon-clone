64643b31dec999ddf80193090256a733d38d91de
Jarosław Bąk <jarodelphi@gmail.com>
2022-05-21 21:12:20 +0200

    Payment method

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import Checkout from './Checkout';
 import { useEffect } from 'react';
 import { auth } from "./firebase"
 import { useStateValue } from './StateProvider';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js"
 
+const promise = loadStripe('pk_test_51L0rNdLhP8cwGCsE70YW56ZHYr5i09iCVump00zzWSvXOAffEtQngH63U3txXfP4Qji5FzlQnIu7ZEaBOAEJG8bQ00pCBvBe7t');
 
 function App() {
   const [{},dispatch] = useStateValue();
@@ -35,10 +38,18 @@ function App() {
     <Router>
       <div className="app">
         <Routes>
-          <Route path='/' element={<><Header /><Home /></>} />
-          <Route path='/checkout' element={<><Header /><Checkout /></>} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/payment' element={<><Header /><Payment /></>} />
+          <Route 
+            path='/' 
+            element={<><Header /><Home /></>} />
+          <Route 
+            path='/checkout' 
+            element={<><Header /><Checkout /></>} />
+          <Route 
+            path='/login' 
+            element={<Login />} />
+          <Route 
+            path='/payment' 
+            element={<><Header /><Elements stripe={promise}><Payment /></Elements></>} />
         </Routes>
       </div>
     </Router>
